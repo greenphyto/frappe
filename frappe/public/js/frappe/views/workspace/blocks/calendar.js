60e95749fe42bc0ab8bd3a8986923ad40bdb8b01
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-28 09:34:15 +0000

    setup calendar wrapper

--- frappe/public/js/frappe/views/workspace/blocks/calendar.js
+++ frappe/public/js/frappe/views/workspace/blocks/calendar.js
@@ -1,49 +1,40 @@
 import Block from "./block.js";
 export default class Calendar extends Block {
-	static get toolbox() {
-		return {
-			title: "Calendar",
-			icon: frappe.utils.icon("calendar", "sm"),
-		};
-	}
+	constructor({ data, config, api, readOnly }) {
+		super({ config, api, readOnly });
 
-	static get isReadOnlySupported() {
-		return true;
-	}
+		this._settings = this.config;
+		this._data = this.normalizeData(data);
+		this._element = this.getTag();
 
-	constructor({ data, api, config, readOnly }) {
-		super({ data, api, config, readOnly });
+		this.data = data;
 		this.col = this.data.col ? this.data.col : "12";
-		this.allow_customization = !this.readOnly;
-		this.options = {
-			allow_sorting: this.allow_customization,
-			allow_create: this.allow_customization,
-			allow_delete: this.allow_customization,
-			allow_hiding: false,
-			allow_edit: true,
-			allow_resize: false,
-			min_width: 12,
-			max_widget_count: 2,
-		};
 	}
 
-    render() {
+	normalizeData(data) {
+		const newData = {};
+
+		if (typeof data !== "object") {
+			data = {};
+		}
+
+		newData.calendar_name = data.calendar_name || "";
+		newData.col = parseInt(data.col) || 12;
+
+		return newData;
+	}
+
+	render() {
 		this.wrapper = document.createElement("div");
-		this.wrapper.classList.add("widget", "calendar");
 		if (!this.readOnly) {
-			let $calendar = $(`
-				<div class="widget-head">
-					<div class="calendar-left"></div>
-					<div>${__("Calendar")}</div>
-					<div class="widget-control"></div>
-				</div>
-			`);
-			$calendar.appendTo(this.wrapper);
+			let $widget_head = $(`<div class="widget-head calendar-mode"></div>`);
+			let $widget_control = $(`<div class="widget-control"></div>`);
 
-			this.wrapper.classList.add("edit-mode");
-			this.wrapper.style.minHeight = 500 + "px";
+			$widget_head[0].appendChild(this._element);
+			$widget_control.appendTo($widget_head);
+			$widget_head.appendTo(this.wrapper);
 
-			let $widget_control = $calendar.find(".widget-control");
+			this.wrapper.classList.add("widget", "calendar", "edit-mode");
 
 			this.add_settings_button();
 			this.add_new_block_button();
@@ -56,13 +47,81 @@ export default class Calendar extends Block {
 				null,
 				$widget_control
 			);
+
+			$(this._element).find(".calendar-name").show();
+			$(this._element).find(".calendar-wrapper").hide();
+			return this.wrapper;
 		}
-		return this.wrapper;
+		$(this._element).find(".calendar-name").hide();
+		$(this._element).find(".calendar-wrapper").show();
+		return this._element;
+	}
+
+	validate(blockData) {
+		return true;
 	}
 
 	save() {
+		this.wrapper = this._element;
+		var element = this.wrapper;
+		var calendar_name = $(element).find(".calendar-name").text();
+		console.log(71, this.wrapper, calendar_name);
+
 		return {
+			calendar_name: calendar_name,
 			col: this.get_col(),
 		};
 	}
+
+	rendered() {
+		super.rendered(this._element);
+	}
+
+	static get isReadOnlySupported() {
+		return true;
+	}
+
+	get data() {
+		var element = $(this._element)
+		this._data.calendar_name = element.find(".calendar-name").text();;
+
+		return this._data;
+	}
+
+	set data(data) {
+		this._data = this.normalizeData(data);
+		if (!this.readOnly && this.wrapper) {
+			this.wrapper.classList.add("widget", "header");
+		}
+	}
+
+	getTag() {
+		const tag = document.createElement("DIV");
+
+		let cls = this._data.calendar_name || "calendar-view";
+		cls = frappe.scrub(cls);
+		tag.innerHTML = `
+			<div class='calendar-name'>${cls}</div>
+			<div class='calendar-wrapper'></div>
+		`;
+
+		tag.classList.add("ce-header");
+		tag.classList.add("ce-calendar");
+		tag.classList.add(cls);
+
+		if (!this.readOnly) {
+			tag.contentEditable = true;
+		}
+
+		return tag;
+	}
+
+	static get toolbox() {
+		return {
+			title: "Calendar",
+			icon: frappe.utils.icon("table", "sm"),
+		};
+	}
 }
+
+console.log("From calendar")
