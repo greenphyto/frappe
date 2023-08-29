d4505e4fb107b7fee5ac20af980fa518e19d3c35
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-08-29 03:12:40 +0000

    add social login url

--- frappe/api.py
+++ frappe/api.py
@@ -11,6 +11,11 @@ import frappe.handler
 from frappe import _
 from frappe.utils.data import sbool
 from frappe.utils.response import build_response
+from frappe.utils.password import get_decrypted_password
+from frappe.utils.oauth import get_oauth2_authorize_url, get_oauth_keys
+from frappe.utils.data import escape_html
+from frappe.utils.html_utils import get_icon_html
+
 
 
 def handle():
@@ -271,3 +276,37 @@ def get_boot_info():
 	user_perms = frappe.utils.user.UserPermissions(frappe.session.user)
 	perm = user_perms.load_user()
 	return perm
+
+@frappe.whitelist()
+@frappe.read_only()
+def get_social_login_providers(redirect_to):
+	providers = frappe.get_all(
+		"Social Login Key",
+		filters={"enable_social_login": 1},
+		fields=["name", "client_id", "base_url", "provider_name", "icon"],
+		order_by="name",
+	)
+	provider_logins = []
+	for provider in providers:
+		client_secret = get_decrypted_password("Social Login Key", provider.name, "client_secret")
+		if not client_secret:
+			continue
+
+		icon = None
+		if provider.icon:
+			if provider.provider_name == "Custom":
+				icon = get_icon_html(provider.icon, small=True)
+			else:
+				icon = f"<img src={escape_html(provider.icon)!r} alt={escape_html(provider.provider_name)!r}>"
+
+		if provider.client_id and provider.base_url and get_oauth_keys(provider.name):
+			provider_logins.append(
+				{
+					"name": provider.name,
+					"provider_name": provider.provider_name,
+					"auth_url": get_oauth2_authorize_url(provider.name, redirect_to),
+					"icon": icon,
+				}
+			)
+
+	return provider_logins
