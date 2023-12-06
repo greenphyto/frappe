57e5736b744e2394848254702c2834bf0b91c146
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2023-12-06 07:26:27 +0000

    send multiple

--- frappe/integrations/firebase.py
+++ frappe/integrations/firebase.py
@@ -16,6 +16,8 @@ FCM_ENDPOINT = 'v1/projects/' + PROJECT_ID + '/messages:send'
 FCM_URL = BASE_URL + '/' + FCM_ENDPOINT
 SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
+cred = credentials.Certificate(frappe.local.conf.firebase_key)
+firebase_admin.initialize_app(cred)
 class FirebaseNotification():
     def __init__(self):
         self.path_key = frappe.local.conf.firebase_key
@@ -52,17 +54,8 @@ class FirebaseNotification():
             return True
         else:
             return False
-
-    def _send_message(self, message, token="", send_all=False, title="Smart FM"):
-        if not token and not send_all:
-            return
-        
-        if token:
-            if len(token) == 1:
-                return self.send_single_message(token[0], message, title)
-
     
-    def send_single_message(self, token, message, title):
+    def send_single_message(self, token, message, title, click_action=None, data={}):
         msg = {
             "message":{
                 "token":token,
@@ -72,17 +65,14 @@ class FirebaseNotification():
                 }
             }
         }
-
         return self.send_fcm_message(msg)
 
-    def send_message(self, message, user, title="Info", click_action=""):
+    def send_message(self, message, user, title="Info", click_action="OPEN_ACTICITY", data={}):
         tokens = frappe.db.get_all("Firebase User Token", filters={"user":user}, fields=["token"])
-        print(tokens) 
         if tokens:
-            tokens = [x.token for x in tokens]
-            print(757575, tokens)
-            res = self._send_message(message, tokens, title=title)
-            print(res)
+            for d in tokens:
+                self.send_single_message(d.token, message, title, click_action, data)
+
             return True
         
         return False
