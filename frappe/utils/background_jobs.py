0f6aef13e7463ae80c59adc187641dfb11171fb2
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-02-07 06:41:23 +0000

    call directly from local

--- frappe/utils/background_jobs.py
+++ frappe/utils/background_jobs.py
@@ -84,7 +84,7 @@ def enqueue(
 			)
 		)
 
-	call_directly = now or frappe.flags.in_migrate or (not is_async and not frappe.flags.in_test)
+	call_directly = now or frappe.flags.in_migrate or (not is_async and not frappe.flags.in_test) or frappe.local.conf.in_test
 	if call_directly:
 		return frappe.call(method, **kwargs)
 
