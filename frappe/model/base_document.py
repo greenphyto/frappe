521f89b6fd7de5d45793ae4acf58cf1667183b79
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-02-19 07:31:30 +0000

    warning and show the doctype

--- frappe/model/base_document.py
+++ frappe/model/base_document.py
@@ -975,7 +975,7 @@ class BaseDocument:
 					)
 				if self_value != db_value:
 					frappe.throw(
-						_("Not allowed to change {0} after submission").format(df.label),
+						_("Not allowed to change {0} after submission ({1})").format(df.label, df.parent),
 						frappe.UpdateAfterSubmitError,
 					)
 
