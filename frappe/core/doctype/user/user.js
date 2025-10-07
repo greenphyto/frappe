d2b0d0942a948f4ee382959bb5ece0af22e171c0
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-10-07 07:50:50 +0000

    adding the company field optional

--- frappe/core/doctype/user/user.js
+++ frappe/core/doctype/user/user.js
@@ -259,6 +259,23 @@ frappe.ui.form.on("User", {
 			}
 			frm.dirty();
 		}
+
+		try {
+			frappe.db.get_list("Company", {
+				fields: ["name"],
+				limit: 1000
+			}).then(records => {
+				let companies = records.map(r => r.name);
+				let f = frm.fields_dict.company;
+				if (f) {
+					f.df.fieldtype = "Select";
+					f.df.options = companies;
+					frm.refresh_field("company");
+				}
+			});
+		} catch (err) {
+			console.warn("⚠️ Failed to load Company list or DocType not available:", err);
+		}
 	},
 	validate: function (frm) {
 		if (frm.roles_editor) {
