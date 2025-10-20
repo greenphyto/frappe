cce94835e7ec1748530d312dd68319042bdd7391
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2025-10-20 03:40:24 +0000

    enable PDF Link

--- frappe/workflow/doctype/workflow_action/workflow_action.py
+++ frappe/workflow/doctype/workflow_action/workflow_action.py
@@ -213,7 +213,7 @@ def return_action_confirmation_page(doc, action, action_link, alert_doc_change=F
 		"alert_doc_change": alert_doc_change,
 	}
 
-	# template_params["pdf_link"] = get_pdf_link(doc.get("doctype"), doc.get("name"))
+	template_params["pdf_link"] = get_pdf_link(doc.get("doctype"), doc.get("name"))
 
 	frappe.respond_as_web_page(
 		title=None,
