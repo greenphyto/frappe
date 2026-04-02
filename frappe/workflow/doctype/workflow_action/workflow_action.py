f5109ace86a49d67eb19d2170c60ef3d30303adf
rizkyGP <139757784+rizkyGP@users.noreply.github.com>
2026-04-02 02:02:11 +0000

    null safe bypass workflow

--- frappe/workflow/doctype/workflow_action/workflow_action.py
+++ frappe/workflow/doctype/workflow_action/workflow_action.py
@@ -555,7 +555,7 @@ def filter_allowed_users(users, doc, transition):
 	from frappe.permissions import has_permission, has_user_permission
 
 	filtered_users = []
-	methods = frappe.get_hooks("bypass_workflow_permission").get(doc.doctype)
+	methods = (frappe.get_hooks("bypass_workflow_permission") or {}).get(doc.doctype) or []
 	for user in users:
 		if has_approval_access(user, doc, transition) and has_permission(doctype=doc, user=user) and has_user_permission(doc, user,1):
 			filtered_users.append(user)
